e87ec18139c304bc3e3f91ee81ef1ebbb28c51ca
Brent Jackson <jxnblk@gmail.com>
2019-03-22 21:50:26 -0400

    Add tests

--- index.test.js
+++ index.test.js
@@ -1,10 +1,13 @@
 /** @jsx mdx */
 import mdx from '@mdx-js/mdx/create-element'
-import React from 'react'
 import renderer from 'react-test-renderer'
+import { matchers } from 'jest-emotion'
 import {
   ComponentProvider,
+  useComponents,
 } from './index'
+
+expect.extend(matchers)
 
 const renderJSON = el => renderer.create(el).toJSON()
 
@@ -29,4 +32,52 @@ test('renders with styles', () => {
     </ComponentProvider>
   )
   expect(json).toMatchSnapshot()
+})
+
+test('renders with theme', () => {
+  const json = renderJSON(
+    <ComponentProvider
+      theme={{
+        colors: {
+          highlight: 'tomato',
+        }
+      }}
+      styles={{
+        h1: props => ({
+          color: props.theme.colors.highlight,
+        })
+      }}>
+      <h1>Hello</h1>
+    </ComponentProvider>
+  )
+})
+
+test('renders with useComponents', () => {
+  let components
+  const Beep = props => {
+    components = useComponents()
+    return false
+  }
+  const json = renderJSON(
+    <ComponentProvider>
+      <Beep />
+    </ComponentProvider>
+  )
+  expect(typeof components).toBe('object')
+  expect(components.h1).toBeTruthy()
+})
+
+test('creates non-standard components', () => {
+  const json = renderJSON(
+    <ComponentProvider
+      styles={{
+        sup: {
+          color: 'tomato'
+        }
+      }}>
+      <sup>hey</sup>
+    </ComponentProvider>
+  )
+  expect(json).toMatchSnapshot()
+  expect(json).toHaveStyleRule('color', 'tomato')
 })
